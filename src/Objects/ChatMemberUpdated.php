c9ec22241844ddbd2d6a2f40d24b943eeaf4e619
Dmytro <dmytro.morozov@aniart.com.ua>
2022-01-14 10:51:35 +0200

    Better typings for chat member

--- src/Objects/ChatMemberUpdated.php
+++ src/Objects/ChatMemberUpdated.php
@@ -10,8 +10,8 @@ use WeStacks\TeleBot\Abstract\TelegramObject;
  * @property Chat $chat Chat the user belongs to
  * @property User $from Performer of the action, which resulted in the change
  * @property integer $date Date the change was done in Unix time
- * @property ChatMember $old_chat_member Previous information about the chat member
- * @property ChatMember $new_chat_member New information about the chat member
+ * @property ChatMemberOwner|ChatMemberAdministrator|ChatMemberMember|ChatMemberRestricted|ChatMemberLeft|ChatMemberBanned $old_chat_member Previous information about the chat member
+ * @property ChatMemberOwner|ChatMemberAdministrator|ChatMemberMember|ChatMemberRestricted|ChatMemberLeft|ChatMemberBanned $new_chat_member New information about the chat member
  * @property ChatInviteLink $invite_link Optional. Chat invite link, which was used by the user to join the chat; for joining by invite link events only.
  */
 class ChatMemberUpdated extends TelegramObject
